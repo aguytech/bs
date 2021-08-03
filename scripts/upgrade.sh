653d6f9188b249600de36ecd6d406ea54132d093
aguy tech <aguytech@free.fr>
2021-08-03 10:23:13 +0200

    upgrade: add centos

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -9,18 +9,18 @@ whiteb="\e[1;1m"; redb="\e[1;31m"; greenb="\e[1;32m"; blueb="\e[1;34m"; magentab
 FILE_RELEASE="/etc/os-release"
 
 ####  manjaro
-if grep -qi manjaro "$FILE_RELEASE"; then
+if grep -qi manjaro "${FILE_RELEASE}"; then
 	cmd="sudo pacman"
 
 	echo -e "${whiteb}pacman update${cclear}"
-	$cmd -Syu  --noconfirm
+	${cmd} -Syu  --noconfirm
 
 	echo -e "${whiteb}pacman clean orphans${cclear}"
-	PCKS="$(pacman -Qdtq)"
-	[ "$PCKS" ] && $cmd -R $PCKS
+	pcks="$(pacman -Qdtq)"
+	[ "${pcks}" ] && ${cmd} -R ${pcks}
 
 	echo -e "${whiteb}pacman clear cache${cclear}"
-	$cmd -Sc --noconfirm
+	${cmd} -Sc --noconfirm
 
 	echo -e "${whiteb}yay update${cclear}"
 	yay -Syu
@@ -33,38 +33,52 @@ if grep -qi manjaro "$FILE_RELEASE"; then
 	rm -fR ~/.cache/yay
 
 ####  ubuntu / debian
-elif grep -qiE 'debian|ubuntu' "$FILE_RELEASE"; then
+elif grep -qiE 'debian|ubuntu' "${FILE_RELEASE}"; then
 
-	grep -qiE 'jessie|xenial|trusty' "$FILE_RELEASE" && cmd="apt-get" || cmd="apt"
-	[[ $USER != root ]] && cmd="sudo $cmd"
+	grep -qiE 'jessie|xenial|trusty' "${FILE_RELEASE}" && cmd="apt-get" || cmd="apt"
+	[[ $USER != root ]] && cmd="sudo ${cmd}"
 
 	echo -e "${whiteb}update${cclear}"
-	$cmd update
+	${cmd} update
 
 	echo -e "${whiteb}upgrade${cclear}"
-	$cmd -y upgrade
+	${cmd} -y upgrade
 
 	echo -e "${whiteb}autoremove${cclear}"
-	$cmd autoremove
+	${cmd} autoremove
 
 	echo -e "${whiteb}clean${cclear}"
-	$cmd -y clean
+	${cmd} -y clean
 
 	echo -e "${whiteb}autoclean${cclear}"
-	$cmd -y autoclean
+	${cmd} -y autoclean
 
 ####  alpine
-elif grep -qiE 'alpine' "$FILE_RELEASE"; then
+elif grep -qiE 'alpine' "${FILE_RELEASE}"; then
 
 	cmd="apk"
 
 	echo -e "${whiteb}update${cclear}"
-	$cmd update
+	${cmd} update
 
 	echo -e "${whiteb}upgrade${cclear}"
-	$cmd upgrade
+	${cmd} upgrade
 
 	echo -e "${whiteb}clean${cclear}"
-	$cmd cache clean
+	${cmd} cache clean
+
+####  centos
+elif grep -qiE 'centos' "${FILE_RELEASE}"; then
+
+	cmd="yum"
+
+	echo -e "${whiteb}update${cclear}"
+	${cmd} -y update
+
+	echo -e "${whiteb}upgrade${cclear}"
+	${cmd} -y upgrade
+
+	echo -e "${whiteb}clean${cclear}"
+	${cmd} -y clean all
 
 fi
