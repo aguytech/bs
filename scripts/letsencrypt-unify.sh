d6b0ff06caa8314d13462e4b51e060df5d17398b
aguy tech <aguytech@free.fr>
2021-11-19 12:33:18 +0100

    letsencrypt-unify: adjust to certbot & openssl

--- scripts/letsencrypt-unify.sh
+++ scripts/letsencrypt-unify.sh
@@ -4,17 +4,32 @@
 # unify certificate and private key
 # & generates a file with list of unified files
 
-path_pem=/etc/server/ssl/private
-path=/etc/letsencrypt/live
+path_share=/var/share/ssl
+path_priv=/etc/server/ssl/private
+path_live=/etc/letsencrypt/live
 
 # unify
-for cert in `ls ${path}/*/ -d`; do
-	cert=`basename ${cert}`
-	cat ${path}/${cert}/fullchain.pem ${path}/${cert}/privkey.pem > ${path_pem}/${cert}.pem
+for domain in `ls ${path_live}/*/ -d 2>/dev/null`; do
+	domain=`basename ${domain}`
+
+	# shared
+	[ "${domain#mail.}" = "${domain}" ] && path=${path_share}/${domain} || path=${path_share}/mail
+	if [[ "${domain#mail.}" = "${domain}" && -d ${path} ]] || [ "${domain#mail.}" != "${domain}" ]; then
+		cp -prL ${path_live}/${domain}/fullchain.pem ${path}/${domain}-fullchain.pem
+		cp -prL ${path_live}/${domain}/privkey.pem ${path}/${domain}-privkey.pem
+	fi
+	
+	# local
+	if [ "${domain#mail.}" = "${domain}" ]; then
+		cp -prL ${path_live}/${domain}/fullchain.pem ${path_priv}/${domain}-fullchain.pem
+		cp -prL ${path_live}/${domain}/privkey.pem ${path_priv}/${domain}-privkey.pem
+		cat ${path_live}/${domain}/fullchain.pem ${path_live}/${domain}/privkey.pem > ${path_priv}/${domain}.pem
+	fi
 done
 
-# generate list of pem files
-echo "$(ls -1 ${path_pem} | grep -v ^letsencrypt)" > ${path_pem}/letsencrypt.pem.lst
-
-# rights
-chmod 600 ${path_pem}/*
+# local: generate list of pem files
+if [ "${domain#mail.}" = "${domain}" ]; then
+	echo "$(ls -1 ${path_priv} | grep -v ^letsencrypt | grep -v '\-privkey.pem')" > ${path_priv}/letsencrypt.pem.lst
+	# rights
+	chmod 600 -R ${path_priv}
+fi
