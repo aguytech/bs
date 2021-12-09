73e030ec24df77b844f99034162f6cf05c6102fc
aguy tech <aguytech@free.fr>
2021-12-09 18:55:01 +0100

    lxd-alpine: modify certbot script with ssl in shared path

--- scripts/certbot-unify.sh
+++ scripts/certbot-unify.sh
@@ -6,6 +6,7 @@
 
 path_ssl_mail=/var/share/ssl/mail
 path_ssl=/var/share/ssl/haproxy
+path_letsencrypt=/var/share/ssl/letsencrypt
 path_live=/etc/letsencrypt/live
 
 # unify
@@ -19,8 +20,8 @@ for domain in `ls ${path_live}/*/ -d 2>/dev/null`; do
 		chmod g=,o= -R ${path}/private
 	fi
 	
-	cat ${path_live}/${domain}/fullchain.pem ${path_live}/${domain}/privkey.pem > ${path_ssl}/haproxy/private/${domain}.pem
+	cat ${path_live}/${domain}/fullchain.pem ${path_live}/${domain}/privkey.pem > ${path_ssl}/haproxy/${domain}.pem
 done
 
 # generate list of pem files
-ls -1 ${path_ssl}/haproxy/private/*.pem > ${path_ssl}/certbot.pem.lst
+ls -1 ${path_ssl}/haproxy/*.pem > ${path_ssl}/certbot.pem.lst
